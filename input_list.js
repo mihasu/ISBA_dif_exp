b9300427622f789a89ea4b3156d85089baf9165a
mihasu <mikael.hasu@fmi.fi>
2023-10-16 07:38:59 +0000

    WebgraF ISBA diff exp

---
+++ input_list.js
@@ -0,0 +1,6 @@
+input_list[0]='Surface.js' 
+input_list[1]='Surface_map.js' 
+input_list[2]='Surface_scat.js' 
+input_list[3]='Surface_skill.js' 
+input_list[4]='Temp.js' 
+input_list[5]='Temp_prof.js' 
